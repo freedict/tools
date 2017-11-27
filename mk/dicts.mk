a076e494037a117eda2dd68b3ba21189f068bb5a
Sebastian Humenda <shumenda@gmx.de>
2017-11-27 16:20:40 +0100

    slob: tei2slob adds version number automatically, so strip it in rule

--- mk/dicts.mk
+++ mk/dicts.mk
@@ -350,7 +350,7 @@ endif
 build-slob: $(BUILD_DIR)/slob/$(dictname)-$(version).slob
 
 $(BUILD_DIR)/slob/$(dictname)-$(version).slob: $(dictname).tei $(BUILD_DIR)/slob
-	tei2slob -w $(BUILD_DIR)/slob -o $@ $<
+	tei2slob -w $(BUILD_DIR)/slob -o $(@:-$(version).slob=.slob) $<
 
 $(call release_path,slob): $(BUILD_DIR)/slob/$(dictname)-$(version).slob $(RELEASE_DIR) 
 	cp $< $@
