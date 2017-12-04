a96b0e96032b155e38d9b5de382acd550bc521e8
Sebastian Humenda <shumenda@gmx.de>
2017-12-04 15:38:33 +0100

    changelog script: abort on empty message

--- buildhelpers/changelog.py
+++ buildhelpers/changelog.py
@@ -76,6 +76,7 @@ def add_changelog_entry(document, edition, date, username, author=None):
 # `<list><item>blah</item></list>`. Please note that you need to take care of
 # escaping yourself.
 """)
+        last_modified = os.path.getmtime(fn)
     ret = os.system('%s %s' % (editor, fn))
     if ret:
         print("Error while starting", editor)
@@ -83,6 +84,9 @@ def add_changelog_entry(document, edition, date, username, author=None):
     with open(fn, encoding='UTF-8') as f:
         data = '\n'.join(l.rstrip() for l in f
                 if l.strip() and not l.lstrip().startswith('#'))
+        if not data.strip():
+            print("No changes, aborting…")
+            sys.exit(0)
     os.remove(fn)
     change = '<change when="{}" who="{}" n="{}">\n'.format(date,
             username, edition)
