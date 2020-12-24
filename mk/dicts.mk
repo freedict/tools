709a0e3d41dbff369384be7b2e690d5ec114a912
Sebastian Humenda <shumenda@gmx.de>
2020-12-24 14:42:18 +0100

    Rewrite teiaddphonetics (Closes #25)
    This is a complete rewrite of the former teiaddphonetics script. The
    former version was falling short in quoting and hence lead to incorrect
    associations between the headword and the IPA transcription.
    It also would consume >  16G of RAM for a 400M dictionary, the new
    version takes only a fraction. A similar improvement has been done for
    the execution performance. The quoting problem is solved as well.

--- mk/dicts.mk
+++ mk/dicts.mk
@@ -95,10 +95,11 @@ dict_tei_source = build/tei/$(dictname)-phonetics.tei
 $(BUILD_DIR)/tei:
 	mkdir -p $@
 
+# Create a copy containing IPA phonetic information
 $(call dict_tei_source): $(dictname).tei | $(BUILD_DIR)/tei
-	$(TEIADDPHONETICS) --infile $< --outfile $@
+	$(TEIADDPHONETICS) -o $@ $<
 else ifeq ($(shell echo '$(supported_phonetics_lang)' |tr -d '[:space:]'|tail -c 1),1)
-dict_tei_source = $(error Espeak or espeak-ng not installed, please install it and proceed.)
+$(error Espeak or espeak-ng not installed, please install it and proceed.)
 endif
 
 ################
